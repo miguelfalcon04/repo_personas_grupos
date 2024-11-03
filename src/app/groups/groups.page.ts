a025a68e968107a53e5146f4e41d4197c68b82fe
Miguel <falconmiguel.2004@gmail.com>
2024-11-03 21:24:46 +0100

    Delete Group added, need to solve refresh()

--- src/app/groups/groups.page.ts
+++ src/app/groups/groups.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Person } from '../core/models/person.model';
 import { Paginated } from '../core/models/paginated.model';
-import { InfiniteScrollCustomEvent } from '@ionic/angular';
+import { InfiniteScrollCustomEvent, ModalController, AlertController } from '@ionic/angular';
 import { Group } from '../core/models/group.model';
 import { GroupService } from '../core/services/impl/group.service';
 
@@ -16,7 +16,11 @@ export class GroupsPage implements OnInit {
   _group:BehaviorSubject<Group[]> = new BehaviorSubject<Group[]>([])
   group$:Observable<Group[]> = this._group.asObservable();
 
-  constructor(private groupSv:GroupService) { }
+  constructor(
+    private groupSv:GroupService,
+    private modalCtrl: ModalController,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit():void {
     this.getMoreGroups();
@@ -24,6 +28,16 @@ export class GroupsPage implements OnInit {
 
   page:number = 1;
   pageSize:number = 25;
+  totalPages!: number;
+
+  refresh(){
+    this.groupSv.getAll(1, (this.page - 1) * this.pageSize).subscribe({
+      next:(response:Paginated<Group>)=>{
+        this.totalPages = response.pages;
+        this._group.next(response.data);
+      }
+    });
+  }
 
   getMoreGroups(notify:HTMLIonInfiniteScrollElement | null = null){
     this.groupSv.getAll(this.page, this.pageSize).subscribe({
@@ -33,6 +47,44 @@ export class GroupsPage implements OnInit {
         notify?.complete();
       }
     })
+  }
+
+
+
+
+  async onAddGroup(){
+    this.modalCtrl
+  }
+
+  async onDeleteGroup(group:Group){
+    const alert = await this.alertController.create({
+      header: "Eliminar grupo",
+      message: "¿Está seguro de que desea eliminar a este grupo?",
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Eliminado cancelado');
+          }
+        },
+        {
+          text: 'Eliminar',
+          handler: () => {
+            console.log('Eliminado confirmado');
+            this.groupSv.delete(group.id).subscribe({
+              next:(deletedGroup) => {
+                console.log(`Grupo eliminada: ${deletedGroup.name}`);
+                this.refresh();
+              }
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
   }
 
 }
